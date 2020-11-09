62cdf6a41711f1c258d7351f314d5b04c4c7d91e
Rafael-main <rafaeladrian.tibudan1201@g.msuiit.edu.ph>
2020-11-09 18:26:34 +0800

    edited content for views

--- student-crud-app/app/views.py
+++ student-crud-app/app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, redirect, jsonify, flash, url_for
+from flask import render_template, request, redirect, jsonify, flash, url_for, json
 from app import app
 import app.models as models
 
@@ -6,35 +6,58 @@ import app.models as models
 @app.route('/index', methods=['GET', 'POST'])
 def index():
     user = models.Student()
+    college = models.College()
+    rows_college = college.read()
+    course = models.Joined()
+    rows_course = course.read_all_course()
+    rows_student = user.read()
+    return render_template('index.html', rows_course=rows_course, rows=rows_student, rows_college=rows_college)
+
+@app.route('/some_list', methods=['GET'])
+def some_list():
+    user = models.Joined()
+    rows_deptcolcor = user.read_on_all()
+    return render_template('dept_course.html', rows=rows_deptcolcor)
+
+@app.route('/_add', methods=['POST'])
+def add():
+    user = models.Student()
+    col = models.Joined()
     if request.method == 'POST':
-        idid = request.values.get('inputID')
-        first = request.values.get('inputFirst')
-        last = request.values.get('inputLast')
-        college = request.values.get('inputCollege')
-        course = request.values.get('inputCourse')
-        year = request.values.get('inputYear')
-        user.create(idid, first, last, college, course, year)
+        idid = request.form['inputID']
+        first = str(request.form['inputFirst']).capitalize()
+        last = str(request.form['inputLast']).capitalize()
+        course = request.form['inputCourse']
+        gender = request.form['inputGender']
+        year = int(request.form['inputYear'])
+        data_col_dept = col.select_college(course)
+        college = data_col_dept[0][0]
+        dept = data_col_dept[0][1]
+
+        
+        user.create(idid, first, last, college, course, gender, year, dept)
         flash("Record Created!", "success")
         return redirect('/')
-    
-    rows = user.read()
-    return render_template('index.html', rows=rows)
 
-@app.route('/edit', methods=['POST'])
+
+@app.route('/_edit', methods=['POST'])
 def edit():
     user = models.Student()
+    col = models.Joined()
     if request.method == 'POST':
-        edit_idid = request.values.get('editInputID')
-        edit_first = request.values.get('editInputFirst')
-        edit_last = request.values.get('editInputLast')
-        edit_college = request.values.get('editInputCollege')
-        edit_course = request.values.get('editInputCourse')
-        edit_year = request.values.get('editInputYear')
-        user.update(edit_idid, edit_first, edit_last, edit_college, edit_course, edit_year)
+        edit_idid = request.form['editInputID']
+        edit_first = str(request.form['editInputFirst']).capitalize()
+        edit_last = str(request.form['editInputLast']).capitalize()
+        edit_course = request.form['editInputCourse']
+        edit_year = int(request.form['editInputYear'])
+        data_col_dept = col.select_college(edit_course)
+        edit_college = data_col_dept[0][0]
+        edit_dept = data_col_dept[0][1]
+        user.update(edit_idid, edit_first, edit_last, edit_college, edit_course, edit_year,edit_dept)
         flash("Record Edited!", "success")
         return redirect('/')
 
-@app.route('/delete', methods=['POST'])
+@app.route('/_delete', methods=['POST'])
 def delete():
     user = models.Student()
     if request.method == 'POST':
